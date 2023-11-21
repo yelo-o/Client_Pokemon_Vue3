db430b1e848988e3782154ecdc96d7794da1fc8d
yelo-o <flyordig@gmail.com>
2023-11-21 16:50:56 +0900

    add alert component

---
+++ src/components/MyAlert.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+
+import { useAlertStore } from '@/stores';
+
+const alertStore = useAlertStore();
+const { alert } = storeToRefs(alertStore);
+
+</script>
+
+<template>
+    <div v-if="alert" class="container">
+        <div class="m-3">
+            <div class="alert alert-dismissable" v-bind:class="alert.type">
+                <button @click="alertStore.clear()" class="btn btn-link close">&times;</button>
+                {{ alert.message }}
+            </div>
+        </div>
+    </div>
+</template>
+
+
