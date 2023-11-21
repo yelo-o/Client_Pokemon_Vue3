5434a10eb476cbdd3c86fe4a167bac2d34a96955
yelo-o <flyordig@gmail.com>
2023-11-21 17:55:14 +0900

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
 </script>
 
 <template>
-  <div></div>
+  <div class="app-container" :class="authStore.user && 'bg-light'">
+    <Nav />
+    <Alert />
+    <div class="container pt-4 pb-4">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style scoped>
