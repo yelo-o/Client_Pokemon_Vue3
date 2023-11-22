4b0ba247db0fee4ab468cc797399b0b007a3c4ae
yelo-o <flyordig@gmail.com>
2023-11-22 09:52:13 +0900

    add user store getAll

--- src/stores/users.store.js
+++ src/stores/users.store.js
@@ -6,11 +6,25 @@ const baseUrl = `${import.meta.env.VITE_API_URL}`;
 export const useUsersStore = defineStore({
     id: 'users',
     state: () => ({
-        user: {}
+        pageInfo: {},
+        users: {},
+        user: {},
     }),
     actions: {
         async register(user) {
             await fetchWrapper.post(`${baseUrl}/auth/register`, user);
+        },
+        async getAll(params) {
+            this.users = { loading: true };
+            try {
+                const queryString = new URLSearchParams(params).toString();
+                const {content, pageSize, totalElements, totalPages } = 
+                    await fetchWrapper.get(`${baseUrl}/admin/users?${queryString}`);
+                this.users = content;   
+                this.pageInfo = {pageSize, totalElements, totalPages}
+            } catch (error) {
+                this.users = { error };
+            }
         },
 
     }
